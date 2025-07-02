72bea7c530a6a124b0fce39dc29215eb97ff6c55
Bhargav009 <36322204+Bhargav009@users.noreply.github.com>
2025-07-02 09:25:44 +0530

    first commit

---
+++ f3.java
@@ -0,0 +1,5 @@
+public class f3 {
+  public static void main(String[] args) {
+    System.out.println("This is f3.java");
+  }
+}
---
+++ f4.java
@@ -0,0 +1,5 @@
+public class f4 {
+  public static void main(String[] args) {
+    System.out.println("This is f4.java");
+  }
+}
